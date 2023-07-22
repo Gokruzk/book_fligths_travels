a6ef5ce94bb5f2c794f07a6efc71255a8358994b
gokrusk <nigelljama@gmail.com>
2023-07-22 13:46:08 -0500

    workflow

---
+++ Proyecto/Controlador/handler.php
@@ -0,0 +1,12 @@
+<?php
+$value = $_GET['value'];
+if ($value == 1) {
+    include("../Vista/admin.html");
+} else if ($value == 2) {
+    include("../Vista/user.html");
+} else if ($value == 3) {
+    include("../Vista/viajes_list.php");
+} else if ($value == 4) {
+    include("../Vista/reportes.php");
+}
+?>
---
+++ Proyecto/Vista/reportes.php
@@ -0,0 +1,31 @@
+<?php
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reportes</title>
+</head>
+<body>
+    <ul>
+        <li>
+            <a href="">Lista de clientes</a>
+        </li>
+        <li>
+            <a href="">Lista de clientes y cantidad de viajes reservados</a>
+        </li>
+        <li>
+            <a href="">Lista de viajes</a>
+        </li>
+        <li>
+            <a href="">Lista de viajes con la cantidad de reseva</a>
+        </li>
+        <li>
+            <a href="">Lista de viajes agrupados por destino</a>
+        </li>
+    </ul>
+</body>
+</html>
