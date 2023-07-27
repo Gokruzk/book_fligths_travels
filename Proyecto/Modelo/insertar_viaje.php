5c5ef60a2922df6b50cc58c388ee55e98fcf9517
Fernando Novillo <70916955+Comtex765@users.noreply.github.com>
2023-07-27 18:41:44 -0500

    cosillas

--- Proyecto/Modelo/insertar_viaje.php
+++ Proyecto/Modelo/insertar_viaje.php
@@ -5,22 +5,38 @@ $origen = $_POST['lugar_origen'];
 $destino = $_POST['lugar_destino'];
 $fecha = $_POST['fecha_viaje'];
 $hora = $_POST['hora_salida'];
-$bus = $_POST['codigo_bus'];
+$placa = $_POST['placa_bus'];
 $precio = $_POST['precio_viaje'];
 
+$sql_placa = "SELECT * FROM Bus WHERE placa = $placa";
 
-$sql = "INSERT INTO Viaje(lugar_origen, lugar_destino,fecha, hora, precio, id_bus) VALUES('$origen', '$destino', '$fecha', '$hora', '$precio', '$bus')";
-$estado =  mysqli_query($conexion, $sql);
+echo "La placa es ", $placa;
 
-if ($estado) {
-    // Redirigir de vuelta a la página con el botón de enviar
-    $return_url = isset($_POST['return_url']) ? $_POST['return_url'] : '../Vista/agregar_viaje.php';
-    header("Location: " . $return_url . "?success=true");
-    exit();
+$existe = mysqli_query($conexion, $sql_placa);
+
+if ($existe) {
+    $sql = "INSERT INTO Viaje(lugar_origen, lugar_destino,fecha, hora, precio, placa) 
+    VALUES ('$origen', '$destino', '$fecha', '$hora', '$precio', '$placa')";
+
+    $estado = mysqli_query($conexion, $sql);
+
+    if ($estado) {
+        // Redirigir de vuelta a la página con el botón de enviar
+        $return_url = isset($_POST['return_url']) ? $_POST['return_url'] : '../Vista/agregar_viaje.php';
+        header("Location: " . $return_url . "?success=true");
+        exit();
+    } else {
+        // Redirigir de vuelta a la página con el botón de enviar
+        $return_url = isset($_POST['return_url']) ? $_POST['return_url'] : '../Vista/agregar_viaje.php';
+        header("Location: " . $return_url . "?success=false");
+        exit();
+    }
 } else {
     // Redirigir de vuelta a la página con el botón de enviar
     $return_url = isset($_POST['return_url']) ? $_POST['return_url'] : '../Vista/agregar_viaje.php';
     header("Location: " . $return_url . "?success=false");
     exit();
-}  
+}
+
+
 ?>
