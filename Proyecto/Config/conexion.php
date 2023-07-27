65ea8457272bf7c2c996e86b4c350e6aab874f3b
gokrusk <nigelljama@gmail.com>
2023-07-27 15:30:49 -0500

    db connect

--- Proyecto/Config/conexion.php
+++ Proyecto/Config/conexion.php
@@ -1,3 +1,8 @@
 <?php
-   $conexion = mysqli_connect('localhost','root','','reserva_viajes');
+$server = 'containers-us-west-180.railway.app';
+$user = 'root';
+$psw = 'a65eDcNWK39UT3qYyPOe';
+$port = '7417';
+$db = 'railway';
+$conexion = mysqli_connect($server,$user,$psw,$db,$port);
 ?>
