6a45112633da5dbda94f2c1d2145f07c86306299
gokrusk <nigelljama@gmail.com>
2023-07-27 22:55:35 -0500

    lista de viajes

--- Proyecto/Vista/viajes_list.php
+++ Proyecto/Vista/viajes_list.php
@@ -8,30 +8,91 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de viajes</title>
     <link rel="stylesheet" href="../../CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="../../CSS/styles.css">
 </head>
 
 <body>
-    <a href="../../index.html">Inicio</a>
-    <h1>Aquí van la lista de viajes</h1>
-    <?php
-    include('../Modelo/mostrar_viajes.php');
-    while ($viaj = $res->fetch_assoc()) {
-        ?>
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">
-                    <?php echo $viaj['lugar_destino'] ?>
-                </h5>
-                <p class="card-text">
-                    <?php echo $viaj['id_viaje'] ?>
-                </p>
-                <a href="../Vista/viaje.php?id_viaje= <?php echo $viaj['id_viaje'] ?>"
-                    class="btn btn-primary">Ver más información</a>
-            </div>
+    <!-- Preloader -->
+    <div class="spinner-wrapper">
+        <div class="spinner">
+            <div class="bounce1"></div>
+            <div class="bounce2"></div>
+            <div class="bounce3"></div>
+        </div>
+    </div>
+    <!-- end of preloader -->
+
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-md navbar-dark navbar-custom fixed-top">
+        <!-- Text Logo - Use this if you don't have a graphic logo -->
+        <!-- <a class="navbar-brand logo-text page-scroll" href="index.html">Aria</a> -->
+
+        <!-- Image Logo -->
+        <img src="../../images/favicon.png" height="60">
+
+        <!-- Mobile Menu Toggle Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+            aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-awesome fas fa-bars"></span>
+            <span class="navbar-toggler-awesome fas fa-times"></span>
+        </button>
+        <!-- end of mobile menu toggle button -->
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link page-scroll" href="../../index.html">INICIO</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="content">
+        <div class="title">
+            <h1>Destinos</h1>
         </div>
         <?php
-    }
-    ?>
+        include('../Modelo/mostrar_viajes.php');
+        while ($viaj = $res->fetch_assoc()) {
+            ?>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <?php echo $viaj['lugar_destino'] ?>
+                    </h5>
+                    <p class="card-text">
+                        <?php echo $viaj['id_viaje'] ?>
+                    </p>
+                    <a href="../Vista/viaje.php?id_viaje= <?php echo $viaj['id_viaje'] ?>" class="btn btn-primary">Ver más
+                        información</a>
+                </div>
+            </div>
+            <?php
+        }
+        ?>
+    </div>
+    <!-- Copyright -->
+    <div class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="p-small">Copyright © 2023 <a href="../../index.html">Transportes de Chimborazo</a></p>
+                </div> <!-- end of col -->
+            </div> <!-- enf of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of copyright -->
+    <!-- end of copyright -->
+    <!-- Scripts -->
+    <script src="../../js/jquery.min.js"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
+    <script src="../../js/popper.min.js"></script> <!-- Popper tooltip library for Bootstrap -->
+    <script src="../../js/bootstrap.min.js"></script> <!-- Bootstrap framework -->
+    <script src="../../js/jquery.easing.min.js"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
+    <script src="../../js/swiper.min.js"></script> <!-- Swiper for image and text sliders -->
+    <script src="../../js/jquery.magnific-popup.js"></script> <!-- Magnific Popup for lightboxes -->
+    <script src="../../js/morphext.min.js"></script> <!-- Morphtext rotating text in the header -->
+    <script src="../../js/isotope.pkgd.min.js"></script> <!-- Isotope for filter -->
+    <script src="../../js/validator.min.js"></script> <!-- Validator.js - Bootstrap plugin that validates forms -->
+    <script src="../../js/scripts.js"></script> <!-- Custom scripts -->
     <script src="../../JS/bootstrap.min.js"></script>
 </body>
 
