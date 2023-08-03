a652b7af3fba372132f78a99b57e92efe1f3d328
Kevin <kevin@gmail>
2023-08-02 21:45:52 -0500

    Agrego comprobante de reserva

---
+++ Proyecto/Modelo/reporte6.php
@@ -0,0 +1,95 @@
+<?php
+$id_reserva = $_GET['value'];
+
+include("../Vista/plantilla6.php");
+include("../Config/conexion.php");
+
+$sql = "SELECT * FROM reserva R
+INNER JOIN usuario U
+ON R.cedula = U.cedula
+INNER JOIN viaje V
+ON R.id_viaje = V.id_viaje
+INNER JOIN asientos A
+ON A.id_reserva = R.id_reserva
+WHERE R.id_reserva = '$id_reserva'";
+$resultado = mysqli_query($conexion, $sql);
+
+$sql1 = "SELECT * FROM reserva R
+INNER JOIN asientos A
+ON A.id_reserva = R.id_reserva
+WHERE R.id_reserva = '$id_reserva'";
+$resultado1 = mysqli_query($conexion, $sql1);
+
+$asientos = '';
+while ($mostrarAsientos = mysqli_fetch_array($resultado1)) {
+    $asientos .= $mostrarAsientos['lugar'] . ', ';
+}
+$asientos = rtrim($asientos, ', ');
+
+//crear objeto 
+$pdf = new PDF();
+$pdf->AliasNbPages();
+
+$pdf->AddPage();
+$pdf->Ln();
+
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->Cell(71, 5, 'Comprobante N ' . $id_reserva, 0, 0);
+
+$pdf->SetFont('Arial', 'I', 10);
+$pdf->Ln(10);
+$mostrar = mysqli_fetch_array($resultado);
+$pdf->Cell(0, 10, 'Fecha reserva: ' . $mostrar['fecha_reserva'], 0, 1);
+$pdf->Cell(0, 10, 'Nombre: ' . $mostrar['nombre'] . ' ' . $mostrar['apellido'], 0, 1);
+$pdf->Cell(0, 10, 'Cédula: ' . $mostrar['cedula'], 0, 1);
+$pdf->Cell(0, 10, 'Correo: ' . $mostrar['correo'], 0, 1);
+$pdf->Cell(0, 10, 'Teléfono: ' .$mostrar['telefono'], 0, 1);
+$pdf->Cell(0, 10, 'Asientos: ' .$asientos, 0, 1);
+$pdf->Ln(10);
+
+// Cabecera de la tabla de productos/servicios
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->Cell(45, 10, 'PLACA DEL BUS', 1, 0, 'C');
+$pdf->Cell(45, 10, 'LUGAR ORIGEN', 1, 0, 'C');
+$pdf->Cell(45, 10, 'LUGAR DESTINO', 1, 0, 'C');
+$pdf->Cell(30, 10, 'FECHA', 1, 0, 'C');
+$pdf->Cell(25, 10, 'HORA', 1, 0, 'C');
+$pdf->Ln();
+
+// Contenido de la tabla de productos/servicios (asumiendo que los datos están en la tabla reserva)
+$pdf->SetFont('Arial', '', 10);
+$pdf->Cell(45, 10, $mostrar['placa'], 1, 0, 'C');
+$pdf->Cell(45, 10, $mostrar['lugar_origen'], 1, 0, 'C');
+$pdf->Cell(45, 10, $mostrar['lugar_destino'], 1, 0, 'C');
+$pdf->Cell(30, 10, $mostrar['fecha'], 1, 0, 'C');
+$pdf->Cell(25, 10, $mostrar['hora'], 1, 0, 'C');
+$pdf->Ln();
+
+// Total a pagar
+$pdf->Ln(10);
+$pdf->SetFont('Arial', 'B', 8);
+
+$precioAdul = $mostrar['precio'];
+$precioNi = number_format(($mostrar['precio'] / 2), 2, '.', '');
+$cantidadAdul = $mostrar['cantidad_adul'];
+$cantidadNi = $mostrar['cantidad_ni'];
+$precioToAdul = $cantidadAdul * $precioAdul;
+$precioToNi = $cantidadNi * $precioNi;
+
+if($cantidadAdul > 1) {
+    $pdf->Cell(0, 10, 'Precio por ' .$cantidadAdul .' adultos: $' .$precioToAdul, 0, 1, 'R');
+    $pdf->Cell(0, 10, 'Precio por ' .$cantidadNi .' niños: $' .$precioToNi, 0, 1, 'R');
+}else {
+    $pdf->Cell(0, 10, 'Precio por ' .$cantidadAdul .' adulto: $' .$precioToAdul, 0, 1, 'R');
+    $pdf->Cell(0, 10, 'Precio por ' .$cantidadNi .' niño: $' .$precioToNi, 0, 1, 'R');
+}
+
+$pdf->SetTextColor(255, 0, 0);
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->Cell(0, 10, 'Total a pagar: $' . $mostrar['precio_total'], 0, 1, 'R');
+$pdf->SetTextColor(0, 0, 0);
+
+// Datos del cliente (asumiendo que los datos del cliente están en la tabla reserva)
+// Mostrar el PDF en el navegador y descargarlo como "factura.pdf"
+$pdf->Output('I', 'comprobante.pdf');
+?>
--- Proyecto/Vista/reservas.php
+++ Proyecto/Vista/reservas.php
@@ -5,10 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Viajes reservados</title>
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:500,700&display=swap&subset=latin-ext"
-        rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600&display=swap&subset=latin-ext"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:500,700&display=swap&subset=latin-ext" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600&display=swap&subset=latin-ext" rel="stylesheet">
 
     <link href="../../CSS/bootstrap.css" rel="stylesheet">
     <link href="../../CSS/fontawesome-all.css" rel="stylesheet">
@@ -32,8 +30,7 @@
     <a href="./user.php?value=<?php echo $rr[0] ?>">
         <button class="btnBack">
             <svg height="16" width="16" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1024 1024">
-                <path
-                    d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z">
+                <path d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z">
                 </path>
             </svg>
             <span>Regresar</span>
@@ -47,7 +44,7 @@
         </div>
         <?php
         while ($viaj = $res->fetch_assoc()) {
-            ?>
+        ?>
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">
@@ -61,13 +58,16 @@
                         <?php echo "Fecha Salida: ";
                         echo $viaj['fecha'] ?>
                     </p>
+                    <a href="../Modelo/reporte6.php?value=<?php echo $viaj['id_reserva'] ?>">
+                        <button class="btnAdmin" style="margin-bottom: 10px;">Comprobante</button>
+                    </a>
                     <button i class="btnGuardar btnAdmin" type="button" name="<?php echo $viaj['id_reserva'] ?>">
                         Cancelar
                     </button>
                     <!-- Modal -->
                 </div>
             </div>
-            <?php
+        <?php
         }
         ?>
         <div style="width:100%">
