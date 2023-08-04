3b4ac5bb8ba198241f9f9a838cdb115c867f7b5c
gokrusk <nigelljama@gmail.com>
2023-08-03 23:00:49 -0500

    cantidad viajes reservados

--- Proyecto/Vista/reservas.php
+++ Proyecto/Vista/reservas.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Viajes reservados</title>
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:500,700&display=swap&subset=latin-ext" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600&display=swap&subset=latin-ext" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:500,700&display=swap&subset=latin-ext"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600&display=swap&subset=latin-ext"
+        rel="stylesheet">
 
     <link href="../../CSS/bootstrap.css" rel="stylesheet">
     <link href="../../CSS/fontawesome-all.css" rel="stylesheet">
@@ -22,17 +24,17 @@
 </head>
 
 <body>
-      <!-- Preloader -->
-  <div class="spinner-wrapper">
-    <div class="spinner">
-      <div class="bounce1"></div>
-      <div class="bounce2"></div>
-      <div class="bounce3"></div>
+    <!-- Preloader -->
+    <div class="spinner-wrapper">
+        <div class="spinner">
+            <div class="bounce1"></div>
+            <div class="bounce2"></div>
+            <div class="bounce3"></div>
+        </div>
     </div>
-  </div>
-  <?php
-  include_once('menu.html');
-  ?>
+    <?php
+    include_once('menu.html');
+    ?>
 
     <?php
     include('../Modelo/mostrar_reservas.php');
@@ -41,7 +43,8 @@
     <a href="./user.php?value=<?php echo $rr[0] ?>">
         <button class="btnBack">
             <svg height="16" width="16" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1024 1024">
-                <path d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z">
+                <path
+                    d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z">
                 </path>
             </svg>
             <span>Regresar</span>
@@ -54,31 +57,37 @@
             </h2>
         </div>
         <?php
-        while ($viaj = $res->fetch_assoc()) {
-        ?>
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <?php echo $viaj['lugar_destino'] ?>
-                    </h5>
-                    <p class="card-text">
-                        <?php echo "Fecha de reserva: ";
-                        echo $viaj['fecha_reserva'] ?>
-                    </p>
-                    <p class="card-text">
-                        <?php echo "Fecha Salida: ";
-                        echo $viaj['fecha'] ?>
-                    </p>
-                    <a href="../Modelo/reporte6.php?value=<?php echo $viaj['id_reserva'] ?>">
-                        <button class="btnAdmin" style="margin-bottom: 10px;">Comprobante</button>
-                    </a>
-                    <button class="btnCancel btnCancelar" type="button" name="<?php echo $viaj['id_reserva'] ?>">
-                        Cancelar
-                    </button>
-                    <!-- Modal -->
+        if (mysqli_num_rows($res) == 0) {
+            ?>
+            <p>No hay viajes reservados</p>
+            <?php
+        } else {
+            while ($viaj = $res->fetch_assoc()) {
+                ?>
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <?php echo $viaj['lugar_destino'] ?>
+                        </h5>
+                        <p class="card-text">
+                            <?php echo "Fecha de reserva: ";
+                            echo $viaj['fecha_reserva'] ?>
+                        </p>
+                        <p class="card-text">
+                            <?php echo "Fecha Salida: ";
+                            echo $viaj['fecha'] ?>
+                        </p>
+                        <a href="../Modelo/reporte6.php?value=<?php echo $viaj['id_reserva'] ?>">
+                            <button class="btnAdmin" style="margin-bottom: 10px;">Comprobante</button>
+                        </a>
+                        <button class="btnCancel btnCancelar" type="button" name="<?php echo $viaj['id_reserva'] ?>">
+                            Cancelar
+                        </button>
+                        <!-- Modal -->
+                    </div>
                 </div>
-            </div>
-        <?php
+                <?php
+            }
         }
         ?>
         <!-- <div style="width:100%">
@@ -88,7 +97,7 @@
         </div> -->
     </div>
 
-        <br>
+    <br>
     <?php
     include_once('footer.html');
     ?>
