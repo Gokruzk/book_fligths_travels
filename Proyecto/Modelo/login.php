db8bb25ff0e52e368e7bbc4efbb618d0c38f865e
gokrusk <nigelljama@gmail.com>
2023-08-03 23:15:06 -0500

    login

--- Proyecto/Modelo/login.php
+++ Proyecto/Modelo/login.php
@@ -4,11 +4,19 @@ $user = $_POST['user'];
 $psw = $_POST['psw'];
 $sql = "SELECT * FROM Usuario WHERE correo = '$user' AND psw = '$psw'";
 $res = mysqli_query($conexion, $sql);
-$us = $res->fetch_assoc();
 
-if ($us['ID_CARGO'] == 1) {
-    include("../Vista/admin.php");
-} else if ($us['ID_CARGO'] == 2) {
-    header("location:../Vista/user.php?value=$user");
+if (mysqli_num_rows($res) == 0) {
+    ?>
+    <script>alert('Correo o contraseña incorrecta');
+    window.location.href = '../../index.html';</script>
+    <?php
+} else {
+    $us = $res->fetch_assoc();
+
+    if ($us['ID_CARGO'] == 1) {
+        include("../Vista/admin.php");
+    } else if ($us['ID_CARGO'] == 2) {
+        header("location:../Vista/user.php?value=$user");
+    }
 }
 ?>
