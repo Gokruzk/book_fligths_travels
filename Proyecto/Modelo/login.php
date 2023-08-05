7324a23e51758262b39553642b444793fe1b7e74
Kevin <kevin@gmail>
2023-08-05 16:13:51 -0500

    Agrego funcionalidades y arreglo login.

--- Proyecto/Modelo/login.php
+++ Proyecto/Modelo/login.php
@@ -2,17 +2,19 @@
 include("../Config/conexion.php");
 $user = $_POST['user'];
 $psw = $_POST['psw'];
-$sql = "SELECT * FROM Usuario WHERE correo = '$user' AND psw = '$psw'";
+
+$sql = "SELECT * FROM Usuario WHERE correo = '$user'";
 $res = mysqli_query($conexion, $sql);
 
-if (mysqli_num_rows($res) == 0) {
+$us = $res->fetch_assoc();
+$hashed_contra = $us['psw'];
+
+if ((mysqli_num_rows($res) == 0) || (password_verify($psw, $hashed_contra))) {
     ?>
     <script>alert('Correo o contraseña incorrecta');
     window.location.href = '../../index.html';</script>
     <?php
 } else {
-    $us = $res->fetch_assoc();
-
     if ($us['ID_CARGO'] == 1) {
         include("../Vista/admin.php");
     } else if ($us['ID_CARGO'] == 2) {
--- Proyecto/Modelo/registrar_usuario.php
+++ Proyecto/Modelo/registrar_usuario.php
@@ -7,16 +7,20 @@ $apellido = $_POST['apellido'];
 $fechaNacimiento = $_POST['fechaNacimiento'];
 $telefono = $_POST['telefono'];
 $correo = $_POST['correo'];
-$contra = sha1($_POST['contra']);
+$contra = $_POST['contra'];
 $cargo = $_POST['cargo'];
+
+$hashed_contra = password_hash($contra, PASSWORD_DEFAULT);
 
 $consulta = "SELECT * FROM usuario WHERE cedula = '$cedula'";
 $resultado = mysqli_query($conexion, $consulta);
 
-if (mysqli_num_rows($resultado) > 0) {
+$correoConsul = $resultado->fetch_assoc();
+
+if ((mysqli_num_rows($resultado) > 0) || ($correoConsul['correo'] == $correo)) {
     echo "ya registrada";
 } else {
-    $sql = "INSERT INTO usuario (cedula, nombre, apellido, fecha_nacimiento, correo, psw, telefono, ID_CARGO) VALUES('$cedula', '$nombre', '$apellido', '$fechaNacimiento', '$correo', '$contra', '$telefono', '$cargo')";
+    $sql = "INSERT INTO usuario (cedula, nombre, apellido, fecha_nacimiento, correo, psw, telefono, ID_CARGO) VALUES('$cedula', '$nombre', '$apellido', '$fechaNacimiento', '$correo', '$hashed_contra', '$telefono', '$cargo')";
     $estado = mysqli_query($conexion, $sql);
 
     if ($estado) {
@@ -25,4 +29,3 @@ if (mysqli_num_rows($resultado) > 0) {
         echo "error";
     }
 }
-?>
